4de4fbb6fad657c9a56c5d9e2de206f511d86c23
fangimal <gimalfan@gmail.com>
2021-07-07 17:48:04 +0300

    Создание проекта, настройка UI, добавление скрипта

---
+++ Assets/Scripts/CoinManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CoinManager : MonoBehaviour
+{
+    [SerializeField]
+    Text coinCoint;
+    void Start()
+    {
+        coinCoint.text = "10";
+    }
+
+    
+    void Update()
+    {
+        
+    }
+
+    public void IncreaseCoin()
+    {
+        Debug.Log("Попытка добавить монету.");
+    }
+
+    public void ResetCoin()
+    {
+        Debug.Log("Сброс значений");
+    }
+
+}
