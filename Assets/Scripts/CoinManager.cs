56f279c1e2be68190e8fe1f7709a82b9372925a2
fangimal <gimalfan@gmail.com>
2021-07-08 07:40:30 +0300

    Начал добавлять монеты и писать логику

--- Assets/Scripts/CoinManager.cs
+++ Assets/Scripts/CoinManager.cs
@@ -1,18 +1,42 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
 
 public class CoinManager : MonoBehaviour
 {
     [SerializeField]
-    Text coinCoint;
-    void Start()
-    {
-        coinCoint.text = "10";
-    }
+    Text coinCointText;
+
+    private float coinCount = 0;
 
     
+
+    [HideInInspector]
+    public float CoinCount
+    {
+        get
+        {
+            return PlayerPrefs.GetFloat("coinCount");
+        }
+        set
+        {
+            if (value >= 0)
+            {
+                PlayerPrefs.SetFloat("coinCount", value);
+            }
+            else
+            {
+                Debug.LogError("gold is negative!");
+            }
+        }
+    }
+
+    void Start()
+    {
+
+        coinCointText.text = CoinCount.ToString();
+    }
+
+
     void Update()
     {
         
@@ -20,12 +44,16 @@ public class CoinManager : MonoBehaviour
 
     public void IncreaseCoin()
     {
-        Debug.Log("Попытка добавить монету.");
+        Debug.Log($"Попытка добавить монету.");
+        CoinCount++;
+        coinCointText.text = CoinCount.ToString();
     }
 
     public void ResetCoin()
     {
+        CoinCount = 0;
         Debug.Log("Сброс значений");
+        coinCointText.text = CoinCount.ToString();
     }
 
 }
